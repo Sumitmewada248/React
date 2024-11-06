978e2615d7bb0ebf8aaf0bb74f2abfc76afef6ec
Sumitmewada248 <sumitmewa@gmail.com>
2024-11-06 21:07:00 +0530

    new

--- 06nov/src/App.jsx
+++ 06nov/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter,Routes,Route } from "react-router-dom"
 import Layout from "./Layout"
 import About from "./About"
 import Home from "./Home"
-import Contact from "./contact"
+import Contact from"./contact"
 
 const App=()=>{
 
