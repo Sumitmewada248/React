c4442d6caabea9c867f9acef77338e8bbc62b2e9
Sumitmewada248 <sumitmewa@gmail.com>
2024-11-06 20:52:55 +0530

    06nov

---
+++ 06nov/src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Layout from "./Layout"
+import About from "./About"
+import Home from "./Home"
+import Contact from "./contact"
+
+const App=()=>{
+
+
+
+    return(
+     
+     <>
+     
+     <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Layout/>}>
+      <Route index element={<Home/>}/>
+      <Route path="home" element={<Home/>}/>
+      <Route path="about" element={<About/>}/>
+      <Route path="contact" element={<Contact/>}/>
+
+      </Route>
+
+     </Routes>
+     
+     
+     </BrowserRouter>
+         </>
+       )
+      }
+      export default App;
---
+++ 06nov/src/contact.jsx
@@ -0,0 +1,10 @@
+const Contact=()=>{
+    return(
+
+    <>
+    <h1>This is our Contact Page</h1>
+    </>
+
+    )
+}
+export default Contact;
