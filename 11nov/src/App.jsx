5f04f0265716329249c52ccfd512813f822efd38
Sumitmewada248 <sumitmewa@gmail.com>
2024-11-11 11:34:42 +0530

    map

---
+++ 11nov/src/App.jsx
@@ -0,0 +1,45 @@
+// import student from "./StuData"
+
+// const ans=student.map((item)=>{
+
+// return(
+//   <tr>
+//     <td>{item.rollno}</td>
+//     <td>{item.name}</td>
+//     <td>{item.city}</td>
+//     <td>{item.fees}</td>
+//   </tr>
+// )
+
+// })
+
+import EmpData from "./EmpData"
+import EmpDesign from "./EmpDesig"
+const ans=EmpData.map((key)=> <EmpDesign
+empid={key.empno}
+empname={key.name}
+desi={key.designation}
+sal={key.salary}
+city={key.City}
+/>)
+
+const App=()=>{
+  return(
+     <>
+    {/* /* <table border={"2px solid "}width={"800px"} bgcolor="skyblue" style={{textAlign:"center", color:"red", fontSize:"40px"}}>
+    //   <tr>
+    //     <th>Rollno</th>
+    //     <th>Name</th>
+    //     <th>City</th>
+    //     <th>Fees</th>
+    //   </tr>
+    //   {ans}
+    // </table> */}
+    <div id="div">
+      {ans}
+    </div>
+    
+    </>
+  )
+}
+export default App
---
+++ 11nov/src/EmpDesig.jsx
@@ -0,0 +1,16 @@
+const EmpDesign=(props)=>{
+return(
+    <>
+    <div id="pro">
+    <h1>Id:{props.empid}</h1>
+    <h1>Name:{props.empname}</h1>
+    <h1>Desinamtion:{props.desi}</h1>
+    <h1>Salary:{props.sal}</h1>
+    <h1>City:{props.city}</h1>
+    </div>
+    </>
+)
+
+
+}
+export default EmpDesign;
