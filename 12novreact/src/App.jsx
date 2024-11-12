3ea435ba8f8ca73d530fce0f354fbf2897587596
Sumitmewada248 <sumitmewa@gmail.com>
2024-11-12 11:37:45 +0530

    bootstrap

---
+++ 12novreact/src/App.jsx
@@ -0,0 +1,63 @@
+import { Container } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Carousel from 'react-bootstrap/Carousel';
+
+
+import img1 from "./image/123.jpg"
+import img2 from "./image/125.jpg"
+const App=()=>{
+  return(
+    <>
+    <Container fluid>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+    
+
+    </Container>
+    <Carousel>
+      <Carousel.Item>
+      <img src={img1}  width="1200px"/>
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src={img1}  width="1200px"/>
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src={img2}  width="1200px"/> 
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+   
+ 
+{/* 
+    <img src="public/images/123.jpg"  width="300px"/>
+    <img src="public/images/125.jpg" width="300px"/> */}
+
+    </>
+  )
+}
+
+export default App;
