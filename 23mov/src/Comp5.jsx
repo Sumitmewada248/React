669cf0f676b31a58ede9c9e7f337b277b9001c01
Sumitmewada248 <sumitmewa@gmail.com>
2024-11-23 11:20:37 +0530

    context

---
+++ 23mov/src/Comp5.jsx
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import { myContext } from "./App";
+
+const Comp5=()=>{
+    const {name}=useContext(myContext);
+    return(
+        <>
+        <h1>Comp5 name:{name}</h1>
+        </>
+    )
+}
+export default Comp5;
